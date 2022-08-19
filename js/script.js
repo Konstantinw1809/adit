f969ae1eb8ccbb44586ce4d6da885a63d13f802e
Konstantinw1809 <konstantinw1809@gmail.com>
2022-08-19 21:04:54 +0300

    navbar

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
   $(".header__burger").click(function (event) {
     $(".header__burger, .header__menu").toggleClass("active");
     $("body").toggleClass("lock");
+    $(".header").toggleClass("active");
   }),
     $(".header__menu").click(function (event) {
       $(".header__burger, .header__menu").removeClass("active");
       $("body").removeClass("lock");
+      $(".header").removeClass("active");
     });
 });
