471aec70109c04603f52b1f286c06d550e2ffb38
Konstantinw1809 <konstantinw1809@gmail.com>
2022-08-21 23:47:19 +0300

    tab

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ $(document).ready(function () {
       $(".header").removeClass("active");
     });
 });
+
+function openTab(evt, tab) {
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tab__block");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tab__links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tab).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
